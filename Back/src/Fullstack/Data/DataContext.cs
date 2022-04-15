1a9ab69d6be573c7fa7df0fc429e872ef450fd5e
wirlianny <wirlianny@gmail>
2022-04-14 22:43:14 -0300

    criação data context

---
+++ Back/src/Fullstack/Data/DataContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fullstack.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fullstack.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+            
+        }
+        public DbSet<Evento> Eventos { get; set; }
+    }
+}
