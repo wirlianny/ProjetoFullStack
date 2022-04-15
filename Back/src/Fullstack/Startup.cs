d83830b8ec8bfb0c9f07de42136cc3e3ea4c4980
wirlianny <wirlianny@gmail>
2022-04-14 22:43:34 -0300

    criação context

--- Back/src/Fullstack/Startup.cs
+++ Back/src/Fullstack/Startup.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Fullstack.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Fullstack
 {
@@ -26,7 +28,9 @@ namespace Fullstack
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<DataContext>(
+                context => context.UseSqlite(Configuration.GetConnectionString("Default"))
+            );
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
